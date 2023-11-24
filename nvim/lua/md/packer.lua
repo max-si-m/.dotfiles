1b010f0c4736c4cc5f1f93d0845acd4e269fc351
Maxim Dzhulii <mak7.dj@gmail.com>
2023-11-24 18:38:23 +0200

    add lsp-zero

--- nvim/lua/md/packer.lua
+++ nvim/lua/md/packer.lua
@@ -15,5 +15,25 @@ return require('packer').startup(function(use)
   use({ 'rose-pine/neovim', as = 'rose-pine' })
 
   use('ThePrimeagen/vim-be-good')
-  use('nvim-treesitter/nvim-treesitter', {run= ':TSUpdate'})
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    requires = {
+      --- Uncomment these if you want to manage LSP servers from neovim
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+		  {'hrsh7th/cmp-path'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'}
+    }
+  }
 end)
