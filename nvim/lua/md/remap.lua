0aaf0ceb791eb10ea5ecb85971d7af0870aee519
Maxim Dzhulii <mak7.dj@gmail.com>
2023-11-26 18:37:37 +0200

    add remap for copy into system clipboard

--- nvim/lua/md/remap.lua
+++ nvim/lua/md/remap.lua
@@ -1,2 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- next greatest remap ever : asbjornHaland
+-- copy to the system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
