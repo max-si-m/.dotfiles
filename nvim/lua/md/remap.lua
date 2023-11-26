c810bea72808c95808bc0d478272827d415b9d21
Maxim Dzhulii <mak7.dj@gmail.com>
2023-11-26 18:41:31 +0200

    add scrolling remaps

--- nvim/lua/md/remap.lua
+++ nvim/lua/md/remap.lua
@@ -5,3 +5,14 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- copy to the system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- move lines up/down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- scroll page but keep coursor in the middle of the screen
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
