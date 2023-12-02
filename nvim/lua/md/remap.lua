f802d0d26f4fb940382923c6d4b7b0145b62113b
Maxim Dzhulii <mak7.dj@gmail.com>
2023-12-02 22:18:36 +0200

    add few remaps

--- nvim/lua/md/remap.lua
+++ nvim/lua/md/remap.lua
@@ -16,3 +16,19 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- greatest remap ever: paste text without overwriting the clipboard
+vim.keymap.set("x", "<leader>p", [["_dP]])
+-- greatest remap ever: delete text without overwriting the clipboard
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- source file
+vim.keymap.set("n", "<leader><leader>", function()
+  vim.cmd("so")
+end)
+
+-- search and replace
+vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
