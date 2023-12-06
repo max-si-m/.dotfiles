630fcf0617b1ce4a577d9c375964bc9d4bef45ea
Maxim Dzhulii <mak7.dj@gmail.com>
2023-12-06 10:58:48 +0200

    fix odd indentation in packer.lua

--- nvim/lua/md/packer.lua
+++ nvim/lua/md/packer.lua
@@ -6,9 +6,9 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
+    -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
   }
   use({ 'rose-pine/neovim', as = 'rose-pine' })
 
@@ -47,7 +47,7 @@ return require('packer').startup(function(use)
       -- Autocompletion
       {'hrsh7th/nvim-cmp'},
       {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
+      {'hrsh7th/cmp-path'},
       {'hrsh7th/cmp-nvim-lsp'},
       {'hrsh7th/cmp-nvim-lua'},
       {'L3MON4D3/LuaSnip'},
