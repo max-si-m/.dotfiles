7fe1cb00fc3e0cea9bc5e6a6c07fc88c06147880
Maxim Dzhulii <mak7.dj@gmail.com>
2023-12-08 10:10:46 +0200

    add lua_ls lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -17,7 +17,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'ruby_ls'},
+  ensure_installed = {'tsserver', 'ruby_ls', 'lua_ls'},
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
