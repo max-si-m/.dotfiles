021d7934e08142ceb18fd805ab16a6dd312b5e98
Maxim Dzhulii <mak7.dj@gmail.com>
2023-12-09 14:00:09 +0200

    fix telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -20,4 +20,4 @@ vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' 
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sk', builtin.keymap, { desc = '[S]earch [K]eymap' })
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymap' })
